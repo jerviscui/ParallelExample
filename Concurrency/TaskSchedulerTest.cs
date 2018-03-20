cbf3ffab7fa2646bdac4daa0b5a323b571f9c7cf
Jervis <cuijervis@126.com>
2018-03-20 18:26:16 +0800

    add WpfApp for asynchronous programming

---
+++ Concurrency/TaskSchedulerTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Concurrency
+{
+    public class TaskSchedulerTest : BaseTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var scheduler = new MyScheduler();
+            var taskFactory = new TaskFactory(scheduler);
+
+            taskFactory.StartNew(() => Trace.WriteLine(""));
+        }
+    }
+
+    public class MyScheduler : TaskScheduler
+    {
+        /// <summary>将 <see cref="T:System.Threading.Tasks.Task" /> 排队到计划程序中。</summary>
+        /// <param name="task">要排队的 <see cref="T:System.Threading.Tasks.Task" />。</param>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// <paramref name="task" /> 参数为 null。</exception>
+        protected override void QueueTask(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>确定是否可以在此调用中同步执行提供的 <see cref="T:System.Threading.Tasks.Task" />，如果可以，将执行该任务。</summary>
+        /// <returns>一个布尔值，该值指示是否已以内联方式执行该任务。</returns>
+        /// <param name="task">要执行的 <see cref="T:System.Threading.Tasks.Task" />。</param>
+        /// <param name="taskWasPreviouslyQueued">一个布尔值，该值指示任务之前是否已排队。如果此参数为 True，则该任务以前可能已排队（已计划）；如果为 False，则已知该任务尚未排队，此时将执行此调用，以便以内联方式执行该任务，而不用将其排队。</param>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// <paramref name="task" /> 参数为 null。</exception>
+        /// <exception cref="T:System.InvalidOperationException">已执行的 <paramref name="task" />。</exception>
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>仅对于调试器支持，生成当前排队到计划程序中等待执行的 <see cref="T:System.Threading.Tasks.Task" /> 实例的枚举。</summary>
+        /// <returns>一个允许调试器遍历当前排队到此计划程序中的任务的枚举。</returns>
+        /// <exception cref="T:System.NotSupportedException">此计划程序无法在此时生成排队任务的列表。</exception>
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string[] _fileNames = {
+            "file01.txt",
+            "file02.txt",
+            "file03.txt",
+            "file04.txt",
+            "file05.txt",
+            "file06.txt",
+            "file07.txt",
+            "file08.txt" };
+
+        private List<Task<string>> fileTasks;
+        private const int BufferSize = 0x2000;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            fileTasks = new List<Task<string>>(_fileNames.Length);
+
+            this.Closed += (sender, args) => System.Environment.Exit(0);
+        }
+
+        private void BtnConcate_Click(object sender, RoutedEventArgs e)
+        {
+            //.../bin/Debug/
+            var dir = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            //.../
+            var projDir = Directory.GetParent(dir.TrimEnd('\\'))?.Parent?.FullName;
+            if (projDir == null)
+            {
+                throw new DirectoryNotFoundException();
+            }
+
+            foreach (var fileName in _fileNames)
+            {
+                fileTasks.Add(ReadAllTextAsync(Path.Combine(projDir, fileName)));
+            }
+
+            Task.WaitAll(fileTasks.ToArray());
+
+            foreach (var fileTask in fileTasks)
+            {
+                Console.WriteLine(fileTask.Result);
+                Console.WriteLine("==============");
+            }
+        }
+
+        /// <summary>
+        /// 异步方式读取文件并返回内容
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        /// <exception cref="System.IO.FileNotFoundException"></exception>
+        private Task<string> ReadAllTextAsync(string path)
+        {
+            var fileInfo = new FileInfo(path);
+
+            if (!fileInfo.Exists)
+            {
+                throw new FileNotFoundException(path);
+            }
+
+            byte[] data = new byte[fileInfo.Length];
+
+            var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, BufferSize, true);
+
+            //APM模式异步操作
+            //var iar = stream.BeginRead(data, 0, data.Length, ar => { }, null);
+            //var count = stream.EndRead(iar);
+
+            //create task the first way
+            //var task = Task.Factory.FromAsync(stream.BeginRead, stream.EndRead, data, 0, data.Length, null,
+            //    TaskCreationOptions.None);
+
+            //must be set FEATURE_ASYNC_IO to use ReadAsync method
+            var task = stream.ReadAsync(data, 0, data.Length);
+
+            return task.ContinueWith(task1 =>
+            {
+                stream.Close();
+                Console.WriteLine(Properties.Resources.ReadAllTextAsync_Completed, task1.Id, task1.Result, stream.Name, DateTime.Now.ToString("HH:mm:ss.ffffff"));
+
+                return task1.Result > 0 ? new UTF8Encoding().GetString(data) : "";
+            }, TaskContinuationOptions.ExecuteSynchronously);
+        }
+    }
+}
